6197260eee78d0dc147b7e5dda88c26e98cead65
coder <dev@torakode.com>
2025-09-22 13:01:59 +0200

    launch script

--- start.sh
+++ start.sh
@@ -41,7 +41,7 @@ case "$1" in
     ;;
   update)
     echo "⬇️ Actualizando proyecto desde git..."
-    git pull origin main || { echo "❌ Error al hacer git pull"; exit 1; }
+    git pull origin master || { echo "❌ Error al hacer git pull"; exit 1; }
     echo "📦 Instalando dependencias..."
     npm install --production || { echo "❌ Error en npm install"; exit 1; }
     echo "🏗 Construyendo la app..."
